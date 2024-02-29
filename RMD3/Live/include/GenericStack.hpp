e77f0e489b4b287e4ca56c8513ca69f65ba2fbd2
Jean-Malo Meichel <jean-malo.meichel@epita.fr>
2024-02-29 08:28:21 +0100

    D3 - Live

--- RMD3/Live/include/GenericStack.hpp
+++ RMD3/Live/include/GenericStack.hpp
@@ -2,20 +2,63 @@
 #define GENERICSTACK_H_
 
 #include <memory>
+#include <iostream>
 
 template <class T>
 class Stack {
-    std::unique_ptr<???> _head = nullptr;
+
+    struct Node {
+        T data;
+        std::unique_ptr<Node> next;
+
+        Node(T d, std::unique_ptr<Node>& n) : data{d}, next{std::move(n)} {}
+    };
+
+    std::unique_ptr<Node> _head = nullptr;
 
     public: // CTOR - DTOR
         Stack() = default;
         ~Stack() = default;
 
     public: // Methods
-        T           pop();
-        void        push(const T& elem);
-        const T&    top() const;
-        T&          top();
+        T           pop() {
+            if (!_head) {
+                throw std::out_of_range("Stack is empty");
+            }
+
+            T data = _head->data;
+            _head = std::move(_head->next);
+            return data;
+        }
+
+        void        push(const T& elem) {
+            _head = std::make_unique<Node>(elem, _head);
+        }
+
+        const T&    top() const {
+            if (!_head) throw std::out_of_range("Stack is empty");
+            return  _head->data;
+        }
+
+        T&          top() {
+            if (!_head) throw std::out_of_range("Stack is empty");
+            return  _head->data;
+        }
+
+        friend std::ostream& operator <<(std::ostream& os, const Stack<T> &st) {
+            // "[1, 2, 3] []"
+            os << "[";
+            auto head = st._head.get();
+            while (head) {
+                os << head->data;
+                if (head->next) {
+                    os << ", ";
+                } 
+                head = head->next.get();
+            }
+            os << "]";
+            return os;
+        }
 };
 
 #endif // GENERICSTACK_H_
